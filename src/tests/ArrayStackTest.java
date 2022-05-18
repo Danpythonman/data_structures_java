9d9703091ef45f68c56da6958038417e37ab4e80
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-05-18 17:39:58 -0400

    Added tests for array-based implementation and linked-list-based implementation of the stack data structure

---
+++ src/tests/ArrayStackTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import stack.Stack;
+import stack.ArrayStack;
+
+class ArrayStackTest {
+
+	@Test
+	void testConstruction() {
+		Stack<Integer> emptyStack = new ArrayStack<Integer>();
+
+		assertTrue(emptyStack.isEmpty());
+		assertEquals(0, emptyStack.size());
+
+		Stack<Integer> oneItemStack = new ArrayStack<Integer>(1);
+
+		assertFalse(oneItemStack.isEmpty());
+		assertEquals(1, oneItemStack.size());
+		assertEquals(1, oneItemStack.top());
+		assertEquals(1, oneItemStack.pop());
+	}
+
+	@Test
+	void testStackPushTopPop() {
+		Stack<String> stack = new ArrayStack<String>("1");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("1", stack.top());
+
+		stack.push("2");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("2", stack.top());
+
+		stack.push("3");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(3, stack.size());
+		assertEquals("3", stack.top());
+
+		assertEquals("3", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("2", stack.top());
+
+		assertEquals("2", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("1", stack.top());
+
+		assertEquals("1", stack.pop());
+
+		assertTrue(stack.isEmpty());
+		assertEquals(0, stack.size());
+
+		stack.push("a");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("a", stack.top());
+
+		stack.push("b");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("b", stack.top());
+
+		assertEquals("b", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("a", stack.top());
+
+		assertEquals("a", stack.pop());
+
+		assertTrue(stack.isEmpty());
+		assertEquals(0, stack.size());
+	}
+
+}
