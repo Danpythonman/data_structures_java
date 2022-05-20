991aaaf49f8ff22b262ffbe59043454306094646
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-05-19 22:25:55 -0400

    Added tests for dynamic circular array based and singly-linked list based implementations of queue.

---
+++ src/tests/ArrayQueueTest.java
@@ -0,0 +1,93 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import queue.ArrayQueue;
+import queue.Queue;
+
+class ArrayQueueTest {
+
+	@Test
+	void testConstruction() {
+		Queue<Integer> emptyQueue = new ArrayQueue<Integer>();
+
+		assertTrue(emptyQueue.isEmpty());
+		assertEquals(0, emptyQueue.size());
+
+		Queue<Integer> oneItemQueue = new ArrayQueue<Integer>(1);
+
+		assertFalse(oneItemQueue.isEmpty());
+		assertEquals(1, oneItemQueue.size());
+		assertEquals(1, oneItemQueue.first());
+		assertEquals(1, oneItemQueue.dequeue());
+	}
+
+	@Test
+	void testStackPushTopPop() {
+		Queue<String> queue = new ArrayQueue<String>("1");
+
+		assertFalse(queue.isEmpty());
+		assertEquals(1, queue.size());
+		assertEquals("1", queue.first());
+
+		queue.enqueue("2");
+
+		assertFalse(queue.isEmpty());
+		assertEquals(2, queue.size());
+		assertEquals("1", queue.first());
+
+		queue.enqueue("3");
+
+		assertFalse(queue.isEmpty());
+		assertEquals(3, queue.size());
+		assertEquals("1", queue.first());
+
+		assertEquals("1", queue.dequeue());
+
+		assertEquals(2, queue.size());
+		assertEquals("2", queue.first());
+
+		assertEquals("2", queue.dequeue());
+
+		assertEquals(1, queue.size());
+		assertEquals("3", queue.first());
+
+		assertEquals("3", queue.dequeue());
+
+		assertTrue(queue.isEmpty());
+		assertEquals(0, queue.size());
+
+		queue.enqueue("a");
+
+		assertEquals(1, queue.size());
+		assertEquals("a", queue.first());
+
+		queue.enqueue("b");
+
+		assertEquals(2, queue.size());
+		assertEquals("a", queue.first());
+
+		queue.enqueue("c");
+
+		assertEquals(3, queue.size());
+		assertEquals("a", queue.first());
+
+		assertEquals("a", queue.dequeue());
+
+		assertEquals(2, queue.size());
+		assertEquals("b", queue.first());
+
+		assertEquals("b", queue.dequeue());
+
+		assertEquals(1, queue.size());
+		assertEquals("c", queue.first());
+
+		assertEquals("c", queue.dequeue());
+
+		assertTrue(queue.isEmpty());
+		assertEquals(0, queue.size());
+	}
+
+}
---
+++ src/tests/LinkedQueueTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import stack.LinkedStack;
+import stack.Stack;
+
+class LinkedQueueTest {
+
+	@Test
+	void testConstruction() {
+		Stack<Integer> emptyStack = new LinkedStack<Integer>();
+
+		assertTrue(emptyStack.isEmpty());
+		assertEquals(0, emptyStack.size());
+
+		Stack<Integer> oneItemStack = new LinkedStack<Integer>(1);
+
+		assertFalse(oneItemStack.isEmpty());
+		assertEquals(1, oneItemStack.size());
+		assertEquals(1, oneItemStack.top());
+		assertEquals(1, oneItemStack.pop());
+	}
+
+	@Test
+	void testStackPushTopPop() {
+		Stack<String> stack = new LinkedStack<String>("1");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("1", stack.top());
+
+		stack.push("2");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("2", stack.top());
+
+		stack.push("3");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(3, stack.size());
+		assertEquals("3", stack.top());
+
+		assertEquals("3", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("2", stack.top());
+
+		assertEquals("2", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("1", stack.top());
+
+		assertEquals("1", stack.pop());
+
+		assertTrue(stack.isEmpty());
+		assertEquals(0, stack.size());
+
+		stack.push("a");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("a", stack.top());
+
+		stack.push("b");
+
+		assertFalse(stack.isEmpty());
+		assertEquals(2, stack.size());
+		assertEquals("b", stack.top());
+
+		assertEquals("b", stack.pop());
+
+		assertFalse(stack.isEmpty());
+		assertEquals(1, stack.size());
+		assertEquals("a", stack.top());
+
+		assertEquals("a", stack.pop());
+
+		assertTrue(stack.isEmpty());
+		assertEquals(0, stack.size());
+	}
+
+}
